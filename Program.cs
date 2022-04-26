c642cf1607f5aeed844563f553f21f5dc4e03b5c
ayariii <90261694+ayariii@users.noreply.github.com>
2022-04-26 14:18:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+using System.Linq;
+
+class Sort { 
+    public static void Main() 
+    { 
+        int[] arr = new int[] {2, 10, 5, 8, 4, 11}; 
+        Console.WriteLine("Array Before Sorting:\n");
+        foreach(int value in arr) 
+        { 
+            Console.Write(value + " "); 
+        } 
+        Console.WriteLine("\n");
+        arr = arr.OrderByDescending(c => c).ToArray();
+        Console.WriteLine("Array After Sorting:\n"); 
+        foreach(int value in arr) 
+        { 
+            Console.Write(value + " "); 
+        } 
+    } 
+} 
